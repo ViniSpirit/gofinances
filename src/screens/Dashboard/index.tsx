dddfcbc721b7b96ca00d61496c64905fca630e1c
Marcos Vinicius <marcosvims@gmail.com>
2022-05-26 15:52:50 -0300

    new branch

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -4,9 +4,10 @@ import { Container, Title } from "./styles"
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard1</Title>
-      <Title>Dashboard2</Title>
+      <Title>Dashboard4</Title>
       <Title>Dashboard3</Title>
+      <Title>Dashboard2</Title>
+      <Title>Dashboard1</Title>
     </Container>
   )
 }
