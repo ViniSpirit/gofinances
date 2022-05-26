e59dec0b0948f1c30dfad137a3b25d55444e1d34
Marcos Vinicius <marcosvims@gmail.com>
2022-05-26 12:25:08 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { ThemeProvider } from "styled-components"
+import { Dashboard } from "./src/screens/Dashboard"
+import theme from "./src/global/styles/theme"
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  )
+}
---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native"
+import { Container, Title } from "./styles"
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Title>Dashboarewesd</Title>
+    </Container>
+  )
+}
